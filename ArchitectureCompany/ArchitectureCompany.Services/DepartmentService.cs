5de10774b2e40d363071aee27b6e203df5478a0a
salihe <salihe.bodeva10@gmail.com>
2023-02-26 08:28:34 +0200

    DepartmentService creation

---
+++ ArchitectureCompany/ArchitectureCompany.Services/DepartmentService.cs
@@ -0,0 +1,36 @@
+﻿using ArchitectureCompany.Data;
+using System;
+
+namespace ArchitectureCompany.Services
+{
+    using ArchitectureCompany.Data;
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class DepartmentService
+    {
+        private AppDbContext context;
+
+        public Department GetDepartmentById(string id)
+        {
+            if (string.IsNullOrWhiteSpace(id))
+            {
+                throw new ArgumentException("Department id is not found!");
+            }
+            if (!int.TryParse(id, out _))
+            {
+                throw new ArgumentException("Invalid department id!");
+            }
+            if (int.Parse(id)<0)
+            {
+                throw new ArgumentException("Invalid department id!");
+            }
+            using (context = new AppDbContext())
+            {
+                Department d = context.Departments.Find(id);
+                return d;
+            }
+        }
+    }
+}
