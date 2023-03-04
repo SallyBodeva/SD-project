a730bd855472fa883f00cf834ea830a0a4674493
Avishe05 <avisheboshnakova@gmail.com>
2023-03-04 18:22:40 +0200

    on AdditionalService creating

---
+++ ArchitectureCompany/ArchitectureCompany.Services/AdditionalService.cs
@@ -0,0 +1,24 @@
+﻿
+
+namespace ArchitectureCompany.Services
+{
+    using ArchitectureCompany.Data;
+    using ArchitectureCompany.Models;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    public class AdditionalService
+    {
+        private AppDbContext context;
+        public Town GetTownById(int id)
+        {
+            if (id < 0)
+            {
+                throw new ArgumentException("Invalid Town id!");
+            }
+            Town town = this.context.Towns.FirstOrDefault(x => x.Id == (id));
+            return town;
+        }
+    }
+}
