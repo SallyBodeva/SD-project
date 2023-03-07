c062d94704e1c9ca2f0d0d8dde28b9ce47e3fff5
Avishe05 <avisheboshnakova@gmail.com>
2023-03-07 19:02:09 +0200

    on Seeder creating

--- ArchitectureCompany/ArchitectureCompany.Seeder/Program.cs
+++ ArchitectureCompany/ArchitectureCompany.Seeder/Program.cs
@@ -10,6 +10,7 @@ namespace ArchitectureCompany.Seeder
     {
         private static AdditionalService addiotionalService = new AdditionalService();
         private static DepartmentService departmentService = new DepartmentService();
+        private static ProjectService projectService = new ProjectService();
         static void Main()
         {
             SeedTowns();
@@ -17,7 +18,7 @@ namespace ArchitectureCompany.Seeder
 
         List<Project> projects = new List<Project>();
         List<BuildingType> buildingTypes = new List<BuildingType>();
-        
+
         public static void SeedDepartments()
         {
             List<string> d = new List<string>();
@@ -39,20 +40,20 @@ namespace ArchitectureCompany.Seeder
         public static void SeedAddresses()
         {
             List<string> a = new List<string>();
-            
+
         }
         public static void SeedEmployee()
         {
-            List<string> firstName = new List<string>() { "Jane", "Lenore", "Susy", "Genna", "Viki", "Toni","Serkan","Eda","Kiraz","Selin","Engin","Steven","Barbara","Kiara","Kerem","Aleph","Quintessa","Margarette","Oscar","Sebastian","Zane","Mabel","Leo","Scarlett","Yasemin","Caleb" };
-            List<string> lastName = new List<string>() { "Stove", "Lobile", "Dykas", "Larne", "Romera", "Borrel","Bolat","Yaldiz","Karadaa", "Smith", "Williams", "Johnson", "Brown", "Jones", "Garcia", "Miller", "Davis", "Rodriguez", "Martinez", "Hernandez", "Lopez", "Gonzalez", "Wilson", "Anderson" };
-            
+            List<string> firstName = new List<string>() { "Jane", "Lenore", "Susy", "Genna", "Viki", "Toni", "Serkan", "Eda", "Kiraz", "Selin", "Engin", "Steven", "Barbara", "Kiara", "Kerem", "Aleph", "Quintessa", "Margarette", "Oscar", "Sebastian", "Zane", "Mabel", "Leo", "Scarlett", "Yasemin", "Caleb" };
+            List<string> lastName = new List<string>() { "Stove", "Lobile", "Dykas", "Larne", "Romera", "Borrel", "Bolat", "Yaldiz", "Karadaa", "Smith", "Williams", "Johnson", "Brown", "Jones", "Garcia", "Miller", "Davis", "Rodriguez", "Martinez", "Hernandez", "Lopez", "Gonzalez", "Wilson", "Anderson" };
+
             Random random = new Random();
 
             for (int i = 0; i < 50; i++)
             {
                 //int employeeFirstName = random.Next(0, firstName.Count);
                 //int employeeLastName = random.Next(0, lastName.Count);
-                
+
                 //Console.WriteLine(EmployeeService.AddEmployee(firstName[employeeFirstName], lastName [employeeLastName]));
             }
         }
@@ -62,13 +63,13 @@ namespace ArchitectureCompany.Seeder
         }
 
         public static void SeedTowns()
-        { 
+        {
             List<string> t = new List<string>();
             t.Add("Plovdiv");
             t.Add("Pazardzhik");
             t.Add("Sofia");
             t.Add("Varna");
-            t.Add("Ruse" );
+            t.Add("Ruse");
             t.Add("Blagoevgrad");
             t.Add("Burgas");
             t.Add("Bansko");
@@ -94,47 +95,52 @@ namespace ArchitectureCompany.Seeder
             t.Add("Lisbon");
             t.Add("Barcelona");
             t.Add("Porto");
+            t.Add("Kabul");
+            t.Add("Tirana");
+            t.Add("Algiers");
+            t.Add("Luanda");
+            t.Add("Yerevan");
+            t.Add("Cambera");
+            t.Add("Baku");
+            t.Add("Bridgetown");
+            t.Add("Minsk");
+            t.Add("Sarajevo");
+            t.Add("Zagreb");
+            t.Add("Havana");
+            t.Add("New Delhi");
+            t.Add("Jakarta");
+            t.Add("Kingston");
+            t.Add("Tokyo");
+            t.Add("Pristina");
+            t.Add("Valleta");
+            t.Add("Wellington");
+            t.Add("Belfast");
+            t.Add("Oslo");
+            t.Add("Warsaw");
+            t.Add("Bern");
+            t.Add("Ankara");
+            t.Add("Puerto Rico");
+            t.Add("Liverpool");
+            t.Add("Santorini");
+            t.Add("Oia");
+            t.Add("Ronda");
+            t.Add("Dobrovnik");
+            t.Add("Sintra");
+            t.Add("New Orleans");
+            t.Add("Coventry");
+            t.Add("Oxford");
+            t.Add("York");
 
-            for (int i = 0; i < 30; i++)
+
+            for (int i = 0; i < 65; i++)
             {
                 string name = t[i];
                 Console.WriteLine(addiotionalService.AddTown(name));
             }
         }
-        // projects.Add(new Project() { Id = 1, Name = "Museum design" });
-        // projects.Add(new Project() { Id = 2, Name = "Theater design" });
-        // projects.Add(new Project() { Id = 3, Name = "Kino design" });
-        // projects.Add(new Project() { Id = 4, Name = "Renovation of old buildings" });
-        // projects.Add(new Project() { Id = 5, Name = "Modern office" });
-        // projects.Add(new Project() { Id = 6, Name = "Modern house" });
-        // projects.Add(new Project() { Id = 7, Name = "Old house" });
-        // projects.Add(new Project() { Id = 8, Name = "Restaurant renovation" });
-        // projects.Add(new Project() { Id = 9, Name = "Renovation of a residential complex" });
-        // projects.Add(new Project() { Id = 10, Name = "Mansion design" });
-        // projects.Add(new Project() { Id = 11, Name = " Leaf fall" });
-        // projects.Add(new Project() { Id = 12, Name = " Oasis" });
-        // projects.Add(new Project() { Id = 13, Name = "Cristal city" });
-        // projects.Add(new Project() { Id = 14, Name = "Golden river" });
-        // projects.Add(new Project() { Id = 15, Name = "Golden street" });
-        // projects.Add(new Project() { Id = 16, Name = "Dream land" });
-        // projects.Add(new Project() { Id = 17, Name = "Children's corner " });
-        // projects.Add(new Project() { Id = 18, Name = "A beaytifull park" });
-        // projects.Add(new Project() { Id = 19, Name = "Subway" });
-        // projects.Add(new Project() { Id = 20, Name = "Bus station" });
-        // projects.Add(new Project() { Id = 21, Name = "Renovation of an old church" });
-        //
-        // buildingTypes.Add(new BuildingType() {TypeName = "house" });
-        // buildingTypes.Add(new BuildingType() {TypeName = "appartmant" });
-        // buildingTypes.Add(new BuildingType() {TypeName = "mansion" });
-        //
-        // buildingTypes.Add(new BuildingType() {TypeName = "farm" });
-        // buildingTypes.Add(new BuildingType() {TypeName = "skyscaper" });
-        // buildingTypes.Add(new BuildingType() {TypeName = "compani" });
-        //
-        // buildingTypes.Add(new BuildingType() {TypeName = "office" });
-        // buildingTypes.Add(new BuildingType() {TypeName = "restaurant" });
-        // buildingTypes.Add(new BuildingType() {TypeName = "kino" });
-        // buildingTypes.Add(new BuildingType() {TypeName = "theater" });
+        
+
+
 
     }
 }
