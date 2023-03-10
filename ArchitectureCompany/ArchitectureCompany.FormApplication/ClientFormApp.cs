4d75e43ddd0a7dfa179c1827775b9d9da023ad42
Avishe05 <avisheboshnakova@gmail.com>
2023-03-10 20:34:54 +0200

    on ClientFormApp creating

---
+++ ArchitectureCompany/ArchitectureCompany.FormApplication/ClientFormApp.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ArchitectureCompany.FormApplication
+{
+    public partial class ClientFormApp : Form
+    {
+        public ClientFormApp()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
