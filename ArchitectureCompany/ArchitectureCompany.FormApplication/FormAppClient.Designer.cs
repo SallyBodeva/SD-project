fbf76e466b28344c07694c8345f2009fdce9d616
Avishe05 <avisheboshnakova@gmail.com>
2023-03-11 13:24:41 +0200

    on Client form app creating

--- ArchitectureCompany/ArchitectureCompany.FormApplication/FormAppClient.Designer.cs
+++ ArchitectureCompany/ArchitectureCompany.FormApplication/FormAppClient.Designer.cs
@@ -28,19 +28,388 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.labelFirstName = new System.Windows.Forms.Label();
+            this.labelLastName = new System.Windows.Forms.Label();
+            this.labelAddress = new System.Windows.Forms.Label();
+            this.labelTown = new System.Windows.Forms.Label();
+            this.labelPhoneNumber = new System.Windows.Forms.Label();
+            this.labelEmail = new System.Windows.Forms.Label();
+            this.tbFirstName = new System.Windows.Forms.TextBox();
+            this.tbLastName = new System.Windows.Forms.TextBox();
+            this.tbAddress = new System.Windows.Forms.TextBox();
+            this.tbTown = new System.Windows.Forms.TextBox();
+            this.tbPhoneNumber = new System.Windows.Forms.TextBox();
+            this.tbEmail = new System.Windows.Forms.TextBox();
+            this.labelEnterInfo = new System.Windows.Forms.Label();
+            this.bSingIn = new System.Windows.Forms.Button();
+            this.labelProject = new System.Windows.Forms.Label();
+            this.labelProjctName = new System.Windows.Forms.Label();
+            this.labelBulidingType = new System.Windows.Forms.Label();
+            this.labelCapasity = new System.Windows.Forms.Label();
+            this.labelArea = new System.Windows.Forms.Label();
+            this.labelFloor = new System.Windows.Forms.Label();
+            this.labelProjectAddress = new System.Windows.Forms.Label();
+            this.labelImage = new System.Windows.Forms.Label();
+            this.tbProjectName = new System.Windows.Forms.TextBox();
+            this.tbCapasity = new System.Windows.Forms.TextBox();
+            this.cbBulidingType = new System.Windows.Forms.ComboBox();
+            this.tbArea = new System.Windows.Forms.TextBox();
+            this.tbFllor = new System.Windows.Forms.TextBox();
+            this.tbProjectAdress = new System.Windows.Forms.TextBox();
+            this.pBImage = new System.Windows.Forms.PictureBox();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.gbBeourClient = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pBImage)).BeginInit();
+            this.gbBeourClient.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // labelFirstName
+            // 
+            this.labelFirstName.AutoSize = true;
+            this.labelFirstName.Location = new System.Drawing.Point(6, 20);
+            this.labelFirstName.Name = "labelFirstName";
+            this.labelFirstName.Size = new System.Drawing.Size(61, 15);
+            this.labelFirstName.TabIndex = 0;
+            this.labelFirstName.Text = "FirstName";
+            this.labelFirstName.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // labelLastName
+            // 
+            this.labelLastName.AutoSize = true;
+            this.labelLastName.Location = new System.Drawing.Point(7, 52);
+            this.labelLastName.Name = "labelLastName";
+            this.labelLastName.Size = new System.Drawing.Size(60, 15);
+            this.labelLastName.TabIndex = 1;
+            this.labelLastName.Text = "LastName";
+            // 
+            // labelAddress
+            // 
+            this.labelAddress.AutoSize = true;
+            this.labelAddress.Location = new System.Drawing.Point(7, 79);
+            this.labelAddress.Name = "labelAddress";
+            this.labelAddress.Size = new System.Drawing.Size(49, 15);
+            this.labelAddress.TabIndex = 2;
+            this.labelAddress.Text = "Address";
+            // 
+            // labelTown
+            // 
+            this.labelTown.AutoSize = true;
+            this.labelTown.Location = new System.Drawing.Point(7, 108);
+            this.labelTown.Name = "labelTown";
+            this.labelTown.Size = new System.Drawing.Size(35, 15);
+            this.labelTown.TabIndex = 3;
+            this.labelTown.Text = "Town";
+            // 
+            // labelPhoneNumber
+            // 
+            this.labelPhoneNumber.AutoSize = true;
+            this.labelPhoneNumber.Location = new System.Drawing.Point(6, 134);
+            this.labelPhoneNumber.Name = "labelPhoneNumber";
+            this.labelPhoneNumber.Size = new System.Drawing.Size(85, 15);
+            this.labelPhoneNumber.TabIndex = 4;
+            this.labelPhoneNumber.Text = "PhoneNumber";
+            // 
+            // labelEmail
+            // 
+            this.labelEmail.AutoSize = true;
+            this.labelEmail.Location = new System.Drawing.Point(20, 166);
+            this.labelEmail.Name = "labelEmail";
+            this.labelEmail.Size = new System.Drawing.Size(36, 15);
+            this.labelEmail.TabIndex = 5;
+            this.labelEmail.Text = "Email";
+            // 
+            // tbFirstName
+            // 
+            this.tbFirstName.Location = new System.Drawing.Point(106, 21);
+            this.tbFirstName.Name = "tbFirstName";
+            this.tbFirstName.Size = new System.Drawing.Size(100, 23);
+            this.tbFirstName.TabIndex = 6;
+            // 
+            // tbLastName
+            // 
+            this.tbLastName.Location = new System.Drawing.Point(106, 75);
+            this.tbLastName.Name = "tbLastName";
+            this.tbLastName.Size = new System.Drawing.Size(100, 23);
+            this.tbLastName.TabIndex = 7;
+            // 
+            // tbAddress
+            // 
+            this.tbAddress.Location = new System.Drawing.Point(106, 46);
+            this.tbAddress.Name = "tbAddress";
+            this.tbAddress.Size = new System.Drawing.Size(100, 23);
+            this.tbAddress.TabIndex = 8;
+            // 
+            // tbTown
+            // 
+            this.tbTown.Location = new System.Drawing.Point(106, 105);
+            this.tbTown.Name = "tbTown";
+            this.tbTown.Size = new System.Drawing.Size(100, 23);
+            this.tbTown.TabIndex = 9;
+            // 
+            // tbPhoneNumber
+            // 
+            this.tbPhoneNumber.Location = new System.Drawing.Point(106, 132);
+            this.tbPhoneNumber.Name = "tbPhoneNumber";
+            this.tbPhoneNumber.Size = new System.Drawing.Size(100, 23);
+            this.tbPhoneNumber.TabIndex = 10;
+            // 
+            // tbEmail
+            // 
+            this.tbEmail.Location = new System.Drawing.Point(106, 163);
+            this.tbEmail.Name = "tbEmail";
+            this.tbEmail.Size = new System.Drawing.Size(100, 23);
+            this.tbEmail.TabIndex = 11;
+            // 
+            // labelEnterInfo
+            // 
+            this.labelEnterInfo.AutoSize = true;
+            this.labelEnterInfo.Location = new System.Drawing.Point(90, 24);
+            this.labelEnterInfo.Name = "labelEnterInfo";
+            this.labelEnterInfo.Size = new System.Drawing.Size(58, 15);
+            this.labelEnterInfo.TabIndex = 12;
+            this.labelEnterInfo.Text = "Enter info";
+            // 
+            // bSingIn
+            // 
+            this.bSingIn.Location = new System.Drawing.Point(45, 288);
+            this.bSingIn.Name = "bSingIn";
+            this.bSingIn.Size = new System.Drawing.Size(197, 60);
+            this.bSingIn.TabIndex = 13;
+            this.bSingIn.Text = "Sing in";
+            this.bSingIn.UseVisualStyleBackColor = true;
+            // 
+            // labelProject
+            // 
+            this.labelProject.AutoSize = true;
+            this.labelProject.Location = new System.Drawing.Point(410, 24);
+            this.labelProject.Name = "labelProject";
+            this.labelProject.Size = new System.Drawing.Size(44, 15);
+            this.labelProject.TabIndex = 14;
+            this.labelProject.Text = "Project";
+            // 
+            // labelProjctName
+            // 
+            this.labelProjctName.AutoSize = true;
+            this.labelProjctName.Location = new System.Drawing.Point(348, 66);
+            this.labelProjctName.Name = "labelProjctName";
+            this.labelProjctName.Size = new System.Drawing.Size(79, 15);
+            this.labelProjctName.TabIndex = 15;
+            this.labelProjctName.Text = "Project Name";
+            // 
+            // labelBulidingType
+            // 
+            this.labelBulidingType.AutoSize = true;
+            this.labelBulidingType.Location = new System.Drawing.Point(348, 92);
+            this.labelBulidingType.Name = "labelBulidingType";
+            this.labelBulidingType.Size = new System.Drawing.Size(78, 15);
+            this.labelBulidingType.TabIndex = 16;
+            this.labelBulidingType.Text = "Buliding Type";
+            // 
+            // labelCapasity
+            // 
+            this.labelCapasity.AutoSize = true;
+            this.labelCapasity.Location = new System.Drawing.Point(354, 117);
+            this.labelCapasity.Name = "labelCapasity";
+            this.labelCapasity.Size = new System.Drawing.Size(52, 15);
+            this.labelCapasity.TabIndex = 17;
+            this.labelCapasity.Text = "Capasity";
+            // 
+            // labelArea
+            // 
+            this.labelArea.AutoSize = true;
+            this.labelArea.Location = new System.Drawing.Point(362, 146);
+            this.labelArea.Name = "labelArea";
+            this.labelArea.Size = new System.Drawing.Size(31, 15);
+            this.labelArea.TabIndex = 18;
+            this.labelArea.Text = "Area";
+            // 
+            // labelFloor
+            // 
+            this.labelFloor.AutoSize = true;
+            this.labelFloor.Location = new System.Drawing.Point(359, 174);
+            this.labelFloor.Name = "labelFloor";
+            this.labelFloor.Size = new System.Drawing.Size(39, 15);
+            this.labelFloor.TabIndex = 19;
+            this.labelFloor.Text = "Floors";
+            // 
+            // labelProjectAddress
+            // 
+            this.labelProjectAddress.AutoSize = true;
+            this.labelProjectAddress.Location = new System.Drawing.Point(349, 203);
+            this.labelProjectAddress.Name = "labelProjectAddress";
+            this.labelProjectAddress.Size = new System.Drawing.Size(49, 15);
+            this.labelProjectAddress.TabIndex = 20;
+            this.labelProjectAddress.Text = "Address";
+            // 
+            // labelImage
+            // 
+            this.labelImage.AutoSize = true;
+            this.labelImage.Location = new System.Drawing.Point(354, 234);
+            this.labelImage.Name = "labelImage";
+            this.labelImage.Size = new System.Drawing.Size(40, 15);
+            this.labelImage.TabIndex = 21;
+            this.labelImage.Text = "Image";
+            // 
+            // tbProjectName
+            // 
+            this.tbProjectName.Location = new System.Drawing.Point(442, 63);
+            this.tbProjectName.Name = "tbProjectName";
+            this.tbProjectName.Size = new System.Drawing.Size(100, 23);
+            this.tbProjectName.TabIndex = 22;
+            // 
+            // tbCapasity
+            // 
+            this.tbCapasity.Location = new System.Drawing.Point(442, 121);
+            this.tbCapasity.Name = "tbCapasity";
+            this.tbCapasity.Size = new System.Drawing.Size(100, 23);
+            this.tbCapasity.TabIndex = 24;
+            // 
+            // cbBulidingType
+            // 
+            this.cbBulidingType.FormattingEnabled = true;
+            this.cbBulidingType.Location = new System.Drawing.Point(434, 92);
+            this.cbBulidingType.Name = "cbBulidingType";
+            this.cbBulidingType.Size = new System.Drawing.Size(121, 23);
+            this.cbBulidingType.TabIndex = 25;
+            // 
+            // tbArea
+            // 
+            this.tbArea.Location = new System.Drawing.Point(442, 150);
+            this.tbArea.Name = "tbArea";
+            this.tbArea.Size = new System.Drawing.Size(100, 23);
+            this.tbArea.TabIndex = 26;
+            // 
+            // tbFllor
+            // 
+            this.tbFllor.Location = new System.Drawing.Point(442, 179);
+            this.tbFllor.Name = "tbFllor";
+            this.tbFllor.Size = new System.Drawing.Size(100, 23);
+            this.tbFllor.TabIndex = 27;
+            // 
+            // tbProjectAdress
+            // 
+            this.tbProjectAdress.Location = new System.Drawing.Point(442, 208);
+            this.tbProjectAdress.Name = "tbProjectAdress";
+            this.tbProjectAdress.Size = new System.Drawing.Size(100, 23);
+            this.tbProjectAdress.TabIndex = 28;
+            // 
+            // pBImage
+            // 
+            this.pBImage.Location = new System.Drawing.Point(442, 237);
+            this.pBImage.Name = "pBImage";
+            this.pBImage.Size = new System.Drawing.Size(100, 50);
+            this.pBImage.TabIndex = 29;
+            this.pBImage.TabStop = false;
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Location = new System.Drawing.Point(373, 293);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(81, 52);
+            this.buttonAdd.TabIndex = 30;
+            this.buttonAdd.Text = "Add";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.Location = new System.Drawing.Point(500, 296);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(87, 52);
+            this.buttonDelete.TabIndex = 31;
+            this.buttonDelete.Text = "Delete";
+            this.buttonDelete.UseVisualStyleBackColor = true;
+            // 
+            // gbBeourClient
+            // 
+            this.gbBeourClient.Controls.Add(this.labelFirstName);
+            this.gbBeourClient.Controls.Add(this.tbFirstName);
+            this.gbBeourClient.Controls.Add(this.labelLastName);
+            this.gbBeourClient.Controls.Add(this.tbLastName);
+            this.gbBeourClient.Controls.Add(this.labelAddress);
+            this.gbBeourClient.Controls.Add(this.tbAddress);
+            this.gbBeourClient.Controls.Add(this.labelTown);
+            this.gbBeourClient.Controls.Add(this.tbTown);
+            this.gbBeourClient.Controls.Add(this.labelPhoneNumber);
+            this.gbBeourClient.Controls.Add(this.tbPhoneNumber);
+            this.gbBeourClient.Controls.Add(this.labelEmail);
+            this.gbBeourClient.Controls.Add(this.tbEmail);
+            this.gbBeourClient.Location = new System.Drawing.Point(20, 63);
+            this.gbBeourClient.Name = "gbBeourClient";
+            this.gbBeourClient.Size = new System.Drawing.Size(293, 224);
+            this.gbBeourClient.TabIndex = 32;
+            this.gbBeourClient.TabStop = false;
+            this.gbBeourClient.Text = "Be our client";
             // 
             // FormAppClient
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(622, 396);
+            this.Controls.Add(this.gbBeourClient);
+            this.Controls.Add(this.buttonDelete);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.pBImage);
+            this.Controls.Add(this.tbProjectAdress);
+            this.Controls.Add(this.tbFllor);
+            this.Controls.Add(this.tbArea);
+            this.Controls.Add(this.cbBulidingType);
+            this.Controls.Add(this.tbCapasity);
+            this.Controls.Add(this.tbProjectName);
+            this.Controls.Add(this.labelImage);
+            this.Controls.Add(this.labelProjectAddress);
+            this.Controls.Add(this.labelFloor);
+            this.Controls.Add(this.labelArea);
+            this.Controls.Add(this.labelCapasity);
+            this.Controls.Add(this.labelBulidingType);
+            this.Controls.Add(this.labelProjctName);
+            this.Controls.Add(this.labelProject);
+            this.Controls.Add(this.bSingIn);
+            this.Controls.Add(this.labelEnterInfo);
             this.Name = "FormAppClient";
             this.Text = "FormAppClient";
+            this.Load += new System.EventHandler(this.FormAppClient_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pBImage)).EndInit();
+            this.gbBeourClient.ResumeLayout(false);
+            this.gbBeourClient.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label labelFirstName;
+        private System.Windows.Forms.Label labelLastName;
+        private System.Windows.Forms.Label labelAddress;
+        private System.Windows.Forms.Label labelTown;
+        private System.Windows.Forms.Label labelPhoneNumber;
+        private System.Windows.Forms.Label labelEmail;
+        private System.Windows.Forms.TextBox tbFirstName;
+        private System.Windows.Forms.TextBox tbLastName;
+        private System.Windows.Forms.TextBox tbAddress;
+        private System.Windows.Forms.TextBox tbTown;
+        private System.Windows.Forms.TextBox tbPhoneNumber;
+        private System.Windows.Forms.TextBox tbEmail;
+        private System.Windows.Forms.Label labelEnterInfo;
+        private System.Windows.Forms.Button bSingIn;
+        private System.Windows.Forms.Label labelProject;
+        private System.Windows.Forms.Label labelProjctName;
+        private System.Windows.Forms.Label labelBulidingType;
+        private System.Windows.Forms.Label labelCapasity;
+        private System.Windows.Forms.Label labelArea;
+        private System.Windows.Forms.Label labelFloor;
+        private System.Windows.Forms.Label labelProjectAddress;
+        private System.Windows.Forms.Label labelImage;
+        private System.Windows.Forms.TextBox tbProjectName;
+        private System.Windows.Forms.TextBox tbCapasity;
+        private System.Windows.Forms.ComboBox cbBulidingType;
+        private System.Windows.Forms.TextBox tbArea;
+        private System.Windows.Forms.TextBox tbFllor;
+        private System.Windows.Forms.TextBox tbProjectAdress;
+        private System.Windows.Forms.PictureBox pBImage;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.Button buttonDelete;
+        private System.Windows.Forms.GroupBox gbBeourClient;
     }
 }
--- ArchitectureCompany/ArchitectureCompany.FormApplication/FormAppClient.cs
+++ ArchitectureCompany/ArchitectureCompany.FormApplication/FormAppClient.cs
@@ -14,5 +14,15 @@ namespace ArchitectureCompany.FormApplication
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FormAppClient_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
