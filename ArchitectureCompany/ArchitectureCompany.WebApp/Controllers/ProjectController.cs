e31af8e0861338a3b1496751706f2a2cffc4d5a2
salihe <salihe.bodeva10@gmail.com>
2023-03-21 22:34:57 +0200

    web app creation

---
+++ ArchitectureCompany/ArchitectureCompany.WebApp/Controllers/ProjectController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ArchitectureCompany.WebApp.Controllers
+{
+    public class ProjectController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ ArchitectureCompany/ArchitectureCompany.WebApp/Models/ProjectIndexViewModel.cs
@@ -0,0 +1,16 @@
+﻿namespace ArchitectureCompany.WebApp.Models
+{
+    public class ProjectIndexViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public string BuildingType { get; set; }
+        public int Capacity { get; set; }
+        public string ReleaseDate { get; set; }
+        public int Area { get; set; }
+        public int FloorsCount { get; set; }
+        public string Address { get; set; }
+        public string ImageUrl { get; set; } = "https://mlcynqtblhhi.i.optimole.com/w:849/h:566/q:mauto/https://i0.wp.com/aplosgroup.com/wp-content/uploads/2018/04/ConstructionProject.jpg?fit=849%2C566&ssl=1";
+    }
+}
